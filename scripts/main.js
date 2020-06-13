7233b7107af8bacc7f152e746b67eff6eccbe8fb
DeltaNedas <deltanedas@gmail.com>
2020-06-13 11:21:00 +0100

    1.4.1: fix crash when breaking reactor on mp

--- scripts/main.js
+++ scripts/main.js
@@ -91,7 +91,7 @@ ui.once(null, () => {
 				}
 			})).margin(4).visible(boolp(() => {
 				// Only show when reactor is low on cryo
-				return reactor.entity != null && reactor.entity.liquids.total() < 28;
+				return reactor.entity != null reactor.entity.liquids != null && reactor.entity.liquids.total() < 28;
 			}));;
 			this.content.add(table);
 			this.content.row();
